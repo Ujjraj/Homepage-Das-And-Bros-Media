ed3ea94e083bf675b6a8a3206f252ec8ad4fb317
Mohil-Ahuja <mohilahuja@gmail.com>
2025-02-27 19:46:06 +0530

    navbar logo route to homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,12 +35,13 @@ const Header = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 flex justify-between items-center w-full">
         {/* LOGO */}
         <div className="flex items-center space-x-2">
-          <img 
-            src="src/assets/logo.png"  
-            alt="Logo" 
-            className="h-12 md:h-16 lg:h-20 w-auto max-h-[50px]"
-          />
-          
+          <Link to="/">
+            <img 
+              src="src/assets/logo.png"  
+              alt="Logo" 
+              className="h-12 md:h-16 lg:h-20 w-auto max-h-[50px]"
+            />
+          </Link>
         </div>
 
         {/* NAVIGATION */}
